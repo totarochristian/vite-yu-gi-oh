50fed9ac70ef71964c79f4d389abf2519c37fd8d
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 18:03:48 +0200

    Add result info component that display the number of card founded

--- src/components/mainAreas/MainComponent.vue
+++ src/components/mainAreas/MainComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="p-4 d-flex flex-column align-items-center gap-3" :class="{'justify-content-center': !store.loadOperationsEnded}">
     <SearchBarComponent v-show="store.loadOperationsEnded" />
+    <ResultInfoComponent v-show="store.loadOperationsEnded" />
     <CardListComponent v-show="store.loadOperationsEnded" />
     <LoaderComponent v-show="!store.loadOperationsEnded" />
   </main>
@@ -9,12 +10,14 @@
 <script>
   import { store } from '../../data/store';
   import SearchBarComponent from '../subAreas/main/SearchBarComponent.vue';
+  import ResultInfoComponent from '../subAreas/main/ResultInfoComponent.vue';
   import CardListComponent from '../subAreas/main/CardsListComponent.vue'
   import LoaderComponent from '../subAreas/main/LoaderComponent.vue';
   export default {
     name: "MainComponent",
     components: {
       SearchBarComponent,
+      ResultInfoComponent,
       CardListComponent,
       LoaderComponent
     },
---
+++ src/components/subAreas/main/ResultInfoComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="badge bg-secondary"><h3>Numero di carte trovate: {{ store.cardsList.length }}</h3></span>
+</template>
+
+<script>
+  import { store } from '../../../data/store';
+  export default {
+    name: "ResultInfoComponent",
+    data(){
+      return {
+        store
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
