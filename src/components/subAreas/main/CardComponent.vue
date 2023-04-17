d0058414633531595b8f7513a5c958612f54ab91
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 17:35:22 +0200

    Add card component and move inside the previous card list content + update the card list to add cardcomponent elements

---
+++ src/components/subAreas/main/CardComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card col-12 col-sm-6 col-md-4 col-lg-2 p-3">
+    <img :src="card.card_images[0].image_url" :alt="'Carta ' + card.name">
+    <h5>{{ card.name }}</h5>
+    <h5>{{ card.archetype }}</h5>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CardComponent",
+    props:{
+      card: Object
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/subAreas/main/CardsListComponent.vue
+++ src/components/subAreas/main/CardsListComponent.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="cardsList" class="w-100 h-100 d-flex justify-content-center align-items-center flex-wrap">
-    <div class="card col-12 col-sm-6 col-md-4 col-lg-2 p-3" v-for="(card,index) in store.cardsList">
-      <img :src="card.card_images[0].image_url" :alt="'Carta ' + card.name">
-      <h5>{{ card.name }}</h5>
-      <h5>{{ card.archetype }}</h5>
-    </div>
+    <CardComponent :card="card" v-for="(card,index) in store.cardsList"/>
   </div>
 </template>
 
 <script>
+  import CardComponent from './CardComponent.vue';
   import { store } from '../../../data/store';
   export default {
     name: "CardListComponent",
@@ -16,6 +13,9 @@
       return {
         store
       }
+    },
+    components:{
+      CardComponent
     }
   }
 </script>
