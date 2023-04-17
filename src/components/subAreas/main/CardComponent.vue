25c41c8a43314b3699d63e319a8c52566dc6c9b1
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 17:37:00 +0200

    Update styles of the cards h5 titles

--- src/components/subAreas/main/CardComponent.vue
+++ src/components/subAreas/main/CardComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card col-12 col-sm-6 col-md-4 col-lg-2 p-3">
     <img :src="card.card_images[0].image_url" :alt="'Carta ' + card.name">
-    <h5>{{ card.name }}</h5>
-    <h5>{{ card.archetype }}</h5>
+    <h5 class="text-center text-uppercase fw-bold pt-3">{{ card.name }}</h5>
+    <h5 class="text-center">{{ card.archetype }}</h5>
   </div>
 </template>
 
