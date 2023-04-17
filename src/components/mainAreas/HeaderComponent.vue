d50a64d0c7ebb2138f3cb3f7967e1c24c2a87428
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 16:05:51 +0200

    Define the html template and the style of the header component

--- src/components/mainAreas/HeaderComponent.vue
+++ src/components/mainAreas/HeaderComponent.vue
@@ -1,5 +1,10 @@
 <template>
-  
+  <header>
+    <div class="w-100 h-100 px-3 py-1 d-flex align-items-center gap-3">
+      <img src="/images/logo.png" alt="Logo Yu-Gi-Oh Api">
+      <h2>Yu-Gi-Oh Api</h2>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,5 +14,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+  header{
+    height: 70px;
+    img{
+      height: 100%;
+      width: auto;
+      object-position: center;
+      object-fit: contain;
+    }
+  }
 </style>
