a42e33b7ef71bbd8adb51829e325fa1b10e7c546
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 16:28:10 +0200

    Add empty card list component and install it inside the main component

--- src/components/mainAreas/MainComponent.vue
+++ src/components/mainAreas/MainComponent.vue
@@ -1,15 +1,18 @@
 <template>
-  <main class="p-4">
+  <main class="p-4 d-flex flex-column justify-content-start align-items-center gap-3">
     <SearchBarComponent />
+    <CardListComponent />
   </main>
 </template>
 
 <script>
 import SearchBarComponent from '../subAreas/main/SearchBarComponent.vue';
+import CardListComponent from '../subAreas/main/CardsListComponent.vue'
   export default {
     name: "MainComponent",
     components: {
-      SearchBarComponent
+      SearchBarComponent,
+      CardListComponent
     }
   }
 </script>
---
+++ src/components/subAreas/main/CardsListComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Card list
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CardListComponent"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
