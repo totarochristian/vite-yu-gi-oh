3eb770e3b3e01ea5d2118bfc1c7ea370fac3e839
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 18:46:01 +0200

    Adjust the style of the description of the cards + add animation up/down

--- src/components/subAreas/main/CardComponent.vue
+++ src/components/subAreas/main/CardComponent.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="cardElem col-12 col-sm-6 col-md-4 col-lg-2 p-3">
     <img :src="card.card_images[0].image_url" :alt="'Carta ' + card.name">
-    <h5 class="text-center text-uppercase fw-bold pt-3">{{ card.name }}</h5>
-    <h5 class="text-center">{{ card.archetype }}</h5>
+    <div class="description">
+      <h5 class="text-center text-uppercase fw-bold pt-3">{{ card.name }}</h5>
+      <h5 class="text-center">{{ card.archetype }}</h5>
+    </div>
   </div>
 </template>
 
@@ -16,12 +18,29 @@
 </script>
 
 <style lang="scss" scoped>
+  @import '../../../assets/styles/partial/variables';
   .cardElem{
-
+    .description{
+      margin-top: 1rem;
+      padding: 1rem;
+      border-radius: 20px;
+      background-color: $cardBackgroundColor;
+      color: $cardColor;
+    }
     img{
       width: 100%;
       height: auto;
       border-radius: 15px;
+      position: relative;
+      animation: UpAndDown 1.5s linear infinite;
+    }
+  }
+  @keyframes UpAndDown {
+    0%, 100% {
+      bottom: 0;
+    }
+    50% {
+      bottom: 10px;
     }
   }
 </style>
