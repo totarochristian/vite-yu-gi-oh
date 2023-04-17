454dc8ea04af21de3b69583170c3169a09c01426
totarochristian <totaro.christian.18@gmail.com>
2023-04-17 16:34:31 +0200

    Import store inside the app and define empty StartLoadOperations method

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,23 @@
 </template>
 
 <script>
+  import store from './data/store';
   import HeaderComponent from './components/mainAreas/HeaderComponent.vue';
-  import MainComponent from './components/mainAreas/MainComponent.vue'
+  import MainComponent from './components/mainAreas/MainComponent.vue';
   export default {
     name: "App",
     components: {
       HeaderComponent,
       MainComponent
     }
+  },
+  methods: {
+    StartLoadOperations(){
+
+    }
+  },
+  mounted: {
+    StartLoadOperations();
   }
 </script>
 
